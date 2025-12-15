ed6c27ac3cb6c077a1ee1c4041cf46b066b88210
puklipo <pcs.engineer.team@gmail.com>
2025-12-15 10:17:32 +0900

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
-* Here is the serverless function entry
-* for deployment with Vercel.
-*/
+ * Here is the serverless function entry
+ * for deployment with Vercel.
+ */
 require __DIR__.'/../public/index.php';
