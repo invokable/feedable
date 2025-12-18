51a4cfeaa928a3aeb4aa5c9df9860ddbc5a6e1f7
kawax <kawaxbiz@gmail.com>
2025-12-18 12:06:08 +0900

    wip

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,11 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+<style>
+    {!! Vite::content('resources/css/app.css') !!}
+</style>
+<script>
+    {!! Vite::content('resources/js/app.js') !!}
+</script>
 @fluxAppearance
