147c8ea81bb5092fac708c033a4d66b1cf71c24c
kawax <kawaxbiz@gmail.com>
2025-12-21 07:18:02 +0900

    Add URI normalization to AbsoluteUri::resolve
    Introduced a normalize method to clean and standardize URIs by removing invalid characters and tags before resolving. This improves URI handling robustness and ensures compliance with RFC 3987.

--- core/Support/AbsoluteUri.php
+++ core/Support/AbsoluteUri.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Revolution\Feedable\Core\Support;
 
+use Illuminate\Support\Str;
 use League\Uri\Uri;
+use League\Uri\UriString;
 
 class AbsoluteUri
 {
+    /**
+     * Range of invalid characters in URI 3987 string.
+     *
+     * From League\Uri\UriString
+     *
+     * @var string
+     */
+    private const string REGEXP_INVALID_URI_RFC3987_CHARS = '/[\x00-\x1f\x7f\s]/';
+
     /**
      * Resolve a relative URI against a base URI.
      *
@@ -20,6 +31,19 @@ class AbsoluteUri
      */
     public static function resolve(string $base, ?string $relative): string
     {
-        return Uri::new(trim(strip_tags($base)))->resolve(trim(strip_tags($relative ?? '')))->toString();
+        return Uri::new(self::normalize($base))
+            ->resolve(self::normalize($relative))
+            ->toString();
+    }
+
+    private static function normalize(?string $uri = null): string
+    {
+        $uri = Str::of($uri)
+            ->stripTags()
+            ->replaceMatches(self::REGEXP_INVALID_URI_RFC3987_CHARS, '')
+            ->trim()
+            ->toString();
+
+        return UriString::normalize($uri);
     }
 }
