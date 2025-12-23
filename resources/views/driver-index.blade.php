c97b65796a59be465bc4e611de7797bf8ee68ccd
puklipo <88759954+puklipo@users.noreply.github.com>
2025-12-23 14:55:52 +0900

    Update driver-index.blade.php

--- resources/views/driver-index.blade.php
+++ resources/views/driver-index.blade.php
@@ -1,5 +1,5 @@
 @foreach($drivers as $driver)
     <div class="pb-3">
-        <flux:link href="#{{ data_get($driver, 'id') }}" variant="ghost" class="text-white">{{ data_get($driver, 'name') }}</flux:link>
+        <flux:link href="#{{ data_get($driver, 'id') }}" variant="ghost" class="text-neutral-300">{{ data_get($driver, 'name') }}</flux:link>
     </div>
 @endforeach
