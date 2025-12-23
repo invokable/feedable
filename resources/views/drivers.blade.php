b216daf4b4bd01576c666f924cba4de43964145d
puklipo <88759954+puklipo@users.noreply.github.com>
2025-12-23 14:43:12 +0900

    Update drivers.blade.php

--- resources/views/drivers.blade.php
+++ resources/views/drivers.blade.php
@@ -10,7 +10,7 @@
             @foreach($drivers as $driver)
                 <div class="mb-3" id="{{ data_get($driver, 'id') }}">
                     <div class="flex flex-col gap-6">
-                        <div class="rounded-xl border bg-white dark:bg-stone-950 dark:border-stone-800 text-stone-800 shadow-xs p-5">
+                        <div class="rounded-xl border bg-white dark:bg-stone-950 dark:border-stone-800  shadow-xs p-5">
                             <flux:heading size="lg">{{ data_get($driver, 'name') }}</flux:heading>
                             <flux:text>{!! Str::markdown(data_get($driver, 'description', ''), ['html_input' => 'strip', 'allow_unsafe_links' => false]) !!}</flux:text>
 
