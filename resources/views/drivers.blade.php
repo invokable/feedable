f08d4da39576b1c8bf7372720da71aed7e3bf475
puklipo <88759954+puklipo@users.noreply.github.com>
2025-12-23 14:50:11 +0900

    Update drivers.blade.php

--- resources/views/drivers.blade.php
+++ resources/views/drivers.blade.php
@@ -9,13 +9,11 @@
         <div class="grid gap-4">
             @foreach($drivers as $driver)
                 <div class="mb-3" id="{{ data_get($driver, 'id') }}">
-                    <div class="flex flex-col gap-6">
-                        <div class="rounded-xl border bg-white dark:bg-stone-950 dark:border-stone-800  shadow-xs p-5">
-                            <flux:heading size="lg">{{ data_get($driver, 'name') }}</flux:heading>
-                            <flux:text>{!! Str::markdown(data_get($driver, 'description', ''), ['html_input' => 'strip', 'allow_unsafe_links' => false]) !!}</flux:text>
+                    <div class="rounded-xl border border-neutral-200 bg-white dark:bg-neutral-900 dark:border-neutral-700 shadow-xs dark:shadow-none p-5">
+                        <flux:heading size="lg">{{ data_get($driver, 'name') }}</flux:heading>
+                        <flux:text>{!! Str::markdown(data_get($driver, 'description', ''), ['html_input' => 'strip', 'allow_unsafe_links' => false]) !!}</flux:text>
 
-                            <flux:input value="{{ url(data_get($driver, 'example')) }}" readonly copyable/>
-                        </div>
+                        <flux:input value="{{ url(data_get($driver, 'example')) }}" readonly copyable/>
                     </div>
                 </div>
             @endforeach
