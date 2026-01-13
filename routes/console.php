52964afb3f55f706aec8fc087eaa453d8d2f11a3
kawax <kawaxbiz@gmail.com>
2026-01-13 22:49:51 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -11,32 +11,3 @@ use Revolution\Feedable\Drivers\JumpPlus\JumpPlusDriver;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::command('jump', function () {
-    $this->comment('Fetching Shonen Jump Plus feed...');
-    $jump = new JumpPlusDriver;
-    dump($jump->handle());
-    $this->comment('Done.');
-})->purpose('Fetch Shonen Jump Plus feed');
-
-// Artisan::command('rsshub-jp', function () {
-//    // RSSHubから日本語ルートを取得してmarkdownファイルを生成
-//    $list = collect(File::glob('../RSSHub/lib/routes/*/namespace.ts'))
-//        ->filter(fn ($file) => str_contains(File::get($file), "lang: 'ja'"))
-//        ->map(fn ($file) => Str::between($file, 'routes/', '/namespace.ts'))
-//        ->map(fn ($name) => "- [ ] {$name}")
-//        ->implode(PHP_EOL);
-//
-//    File::put('docs/routes-jp.md', "# RSSHub 日本語ルート一覧\n\n{$list}\n");
-// });
-
-Artisan::command('rsshub-en', function () {
-    // 一つ上の場所にcloneして実行
-    $list = collect(File::glob('../RSSHub/lib/routes/*/namespace.ts'))
-        ->filter(fn ($file) => str_contains(File::get($file), "lang: 'en'"))
-        ->map(fn ($file) => Str::between($file, 'routes/', '/namespace.ts'))
-        ->map(fn ($name) => "- [ ] {$name}")
-        ->implode(PHP_EOL);
-
-    File::put('docs/routes-en.md', "# RSSHub English routes\n\n{$list}\n");
-});
