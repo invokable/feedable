c6c6812e6a568f01b77b2c545884f227608b7289
kawax <kawaxbiz@gmail.com>
2026-01-13 22:49:53 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Revolution\Feedable\Core\Driver;
 
-// 仮でサイトリストを表示
 Route::get('/', function () {
     return view('drivers')->with([
         'drivers' => Driver::collect()->sortKeys(),
