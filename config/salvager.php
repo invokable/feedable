64f49aed2e0336979d5763dc98bdccc310002aa7
kawax <kawaxbiz@gmail.com>
2026-01-17 18:51:24 +0900

    Create salvager.php

---
+++ config/salvager.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'playwright' => [
+        // Playwright options can be added here.
+
+        // 'headless' => true,
+        // 'args'     => ['--no-sandbox'],
+    ],
+
+    'agent-browser' => [
+        'path' => base_path(env('AGENT_BROWSER_PATH', 'agent-browser')),
+
+        /**
+         * path to chromium
+         */
+        'executable-path' => env('AGENT_BROWSER_EXECUTABLE_PATH'),
+
+        'options' => env('AGENT_BROWSER_OPTIONS'),
+    ],
+
+    'screenshots' => storage_path('salvager/screenshots'),
+];
