527588ff5cd03dd19812424169f54644b2e0247e
kawax <kawaxbiz@gmail.com>
2026-01-17 21:19:56 +0900

    wip

--- api/install.ts
+++
@@ -1,9 +0,0 @@
-import chromium from '@sparticuz/chromium';
-
-export default {
-    async fetch(request: Request) {
-        const path = await chromium.executablePath();
-        console.log(`${path}`);
-        return new Response(`${path}`);
-    },
-};
