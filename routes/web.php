2aa5ea34adf46fe41a03c4b2789e80bc5cb685e1
kawax <kawaxbiz@gmail.com>
2026-01-24 08:25:29 +0900

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Revolution\Feedable\Core\Driver;
+use Revolution\Feedable\Drivers\Note\NoteIndexDriver;
 
 Route::get('/', function () {
     return view('drivers')->with([
         'drivers' => Driver::collect()->sortKeys(),
     ]);
 })->name('home');
+
+Route::get('note', function () {
+    $items = new NoteIndexDriver()->handle();
+    return 'Successfully fetched '.count($items).' items.';
+});
